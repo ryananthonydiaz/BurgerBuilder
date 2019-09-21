9d5fc10ecb9f6165812f06c63686800269c9fa24
ryananthonydiaz <email@ryananthonydiaz.com>
2019-09-20 21:14:40 -0700

    Application Complete with Life Cycle Hooks

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,15 +1,18 @@
 import React from 'react';
 
 import classes from './Toolbar.module.css';
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 
 const Toolbar = props => (
 	<header className={classes.Toolbar}>
-		<div>MENU</div>
-		<div>LOGO</div>
-		<nav>
-			<ul>
-				<li></li>
-			</ul>
+		<DrawerToggle clicked={props.drawerToggleClicked} />
+		<div className={classes.Logo}>
+			<Logo />
+		</div>
+		<nav className={classes.DesktopOnly}>
+			<NavigationItems />
 		</nav>
 	</header>
 );
