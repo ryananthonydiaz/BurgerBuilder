210703daff95a17f49108ca2503535ee37db1151
ryananthonydiaz <email@ryananthonydiaz.com>
2019-09-23 00:34:43 -0700

    Completed all State Management without Redux or MobX. Application complete. Next - Redux integration throughout the Application

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { withRouter } from 'react-router-dom';
 
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 import classes from './Burger.module.css';
@@ -28,4 +27,4 @@ const Burger = props => {
 	);
 };
 
-export default withRouter(Burger);
+export default Burger;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -90,37 +90,19 @@ class BurgerBuilder extends Component {
 		this.setState({ purchasing: false });
 	};
 
-	// purchaseContinueHandler = () => {
-	// 	alert('You continue');
-	// 	this.setState({ loading: true });
-	// 	const order = {
-	// 		ingredients: this.state.ingredients,
-	// 		price: this.state.totalPrice,
-	// 		customer: {
-	// 			name: 'Ryan Diaz',
-	// 			address: {
-	// 				street: '12584 Atwood Ct.',
-	// 				zipCode: '91739',
-	// 				country: 'USA',
-	// 			},
-	// 			email: 'ryvndivz@gmail.com',
-	// 		},
-	// 	};
-	// 	axios
-	// 		.post('/orders.json', order)
-	// 		.then(response =>
-	// 			this.setState({
-	// 				loading: false,
-	// 				purchasing: false,
-	// 			})
-	// 		)
-	// 		.catch(error =>
-	// 			this.setState({
-	// 				loading: false,
-	// 				purchasing: false,
-	// 			})
-	// 		);
-	// };
+	purchaseContinueHandler = () => {
+		const queryParams = [];
+		// eslint-disable-next-line
+		for (let i in this.state.ingredients) {
+			queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
+		}
+		queryParams.push('price=' + this.state.totalPrice);
+		const queryString = queryParams.join('&');
+		this.props.history.push({
+			pathname: '/checkout',
+			search: '?' + queryString,
+		});
+	};
 
 	render() {
 		const disabledInfo = {
