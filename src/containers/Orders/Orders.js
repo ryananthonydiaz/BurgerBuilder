57d88ff876448876de450068db1f9ad51058050a
ryananthonydiaz <email@ryananthonydiaz.com>
2019-09-25 23:30:51 -0700

    Orders page specific to user token auth

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -6,20 +6,33 @@ import axios from '../../axios-orders';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 import { fetchOrders } from '../../store/actions/index';
 import Spinner from '../../components/UI/Spinner/Spinner';
+import classes from './Orders.module.css';
 
 export class Orders extends Component {
 	componentDidMount() {
-		this.props.onFetchOrders(this.props.token);
+		this.props.onFetchOrders(this.props.token, this.props.userId);
 	}
 
 	render() {
 		let orders = <Spinner />;
+
 		if (!this.props.loading) {
 			orders = this.props.orders.map(order => (
 				<Order key={order.id} ingredients={order.ingredients} price={order.price} />
 			));
 		}
-		return <div>{orders}</div>;
+		return (
+			<div>
+				{orders.length === 0 ? (
+					<div className={classes.NoOrder}>
+						<h2 className={classes.title}>No Orders have been placed yet</h2>
+						<p className={classes.body}>Use the menu in the top left corner to place an order</p>{' '}
+					</div>
+				) : (
+					orders
+				)}
+			</div>
+		);
 	}
 }
 
@@ -28,12 +41,13 @@ const mapStateToProps = state => {
 		orders: state.order.orders,
 		loading: state.order.loading,
 		token: state.auth.token,
+		userId: state.auth.userId,
 	};
 };
 
 const mapDispatchToProps = dispatch => {
 	return {
-		onFetchOrders: token => dispatch(fetchOrders(token)),
+		onFetchOrders: (token, userId) => dispatch(fetchOrders(token, userId)),
 	};
 };
 
